f5ea42a3d2c91ae07495fac5a3b03d54ebb498e6
Joda <joda@SBN-MacBookPro.local>
2025-04-28 14:53:17 +0100

    updated code

--- test.ps1
+++ test.ps1
@@ -1,9 +1,1 @@
-$varAzUkAbbrName = 'AZUK'
-$varAzUkSouth = 'S'
-$varLogAnalyticsAbbrName = 'LAW'
-$varLogAnalyticsWorkspaceName = ($varAzUkAbbrName + $varAzUkSouth + '-' + $varLogAnalyticsAbbrName + '-MGT-01').ToUpper()
-
-$varLoggingResourceGroupName = ($varAzUkAbbrName + $varAzUkSouth + '-rg-mgt-log-01').ToUpper()
-    
-
-$parLogAnalyticsWorkspaceResourceId = ('/subscriptions/b0017132-8f81-459f-aead-3171c449866f/resourceGroups/{0}/providers/Microsoft.OperationalInsights/workspaces/{1}' -f $varLoggingResourceGroupName.toUpper(),$varLogAnalyticsWorkspaceName.toUpper()).Split('/')[8]
+hello
